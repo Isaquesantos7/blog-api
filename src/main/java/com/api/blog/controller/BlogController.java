a853ff9cc20187d141bd6671f54113e64dec1ceb
Isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-07-15 18:12:48 -0300

    feat: descrevendo as funcionalidades e status code dos controllers da api

--- src/main/java/com/api/blog/controller/BlogController.java
+++ src/main/java/com/api/blog/controller/BlogController.java
@@ -3,6 +3,9 @@ package com.api.blog.controller;
 import com.api.blog.DTOS.BlogDTO;
 import com.api.blog.entities.Blog;
 import com.api.blog.services.BlogService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +22,12 @@ public class BlogController {
     @Autowired
     private BlogService blogService;
 
+    @Operation(description = "Lista todas as noticias!")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Lista das noticias cadastradas!"),
+            }
+    )
     @GetMapping("/api/blog")
     public ResponseEntity<List<Blog>> findAll() {
         List<Blog> list = this.blogService.listaTodosBlogs();
@@ -26,6 +35,13 @@ public class BlogController {
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
+    @Operation(description = "Listando noticia com base no id passado como parametro!")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Lista noticia com base no Id passado!"),
+                    @ApiResponse(responseCode = "404", description = "Não existe noticia cadastrada com esse Id!")
+            }
+    )
     @GetMapping("/api/blog/{id}")
     public ResponseEntity<Blog> findById(@PathVariable(value = "id") Integer id) {
         Blog blog = this.blogService.blogPorId(id);
@@ -33,6 +49,12 @@ public class BlogController {
         return ResponseEntity.status(HttpStatus.OK).body(blog);
     }
 
+    @Operation
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Noticia cadastrada com sucesso!")
+            }
+    )
     @PostMapping("/api/blog")
     public ResponseEntity<Blog> save(@RequestBody @Valid BlogDTO blogDTO) {
         Blog blog = new Blog();
@@ -41,12 +63,26 @@ public class BlogController {
         return ResponseEntity.status(HttpStatus.CREATED).body(this.blogService.saveBlog(blog));
     }
 
+    @Operation
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Noticia deletada com sucesso!"),
+                    @ApiResponse(responseCode = "404", description = "Noticia não encontrada com esse id!")
+            }
+    )
     @DeleteMapping("/api/blog/{id}")
     public ResponseEntity<Blog> delete(@PathVariable(value = "id") Integer id) {
 
         return ResponseEntity.status(HttpStatus.OK).body(this.blogService.deleteBlog(id));
     }
 
+    @Operation
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Noticia atualizada com sucesso!"),
+                    @ApiResponse(responseCode = "404", description = "Noticia não encontrada com esse id!")
+            }
+    )
     @PutMapping("/api/blog/{id}")
     public ResponseEntity<Blog> update(@PathVariable(value = "id") Integer id, @RequestBody @Valid BlogDTO blogDTO) {
         Blog updateBlog = this.blogService.update(id, blogDTO);
